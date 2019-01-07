e3f7b424da39d2f12ed321068d29112730234523
yleihan <yleihan@163.com>
2019-01-08 01:14:22 +0800

    ligin_template

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,11 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 mail = Mail()
@@ -15,6 +20,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    login_manager.init_app(app)
 
     bootstrap.init_app(app)
     mail.init_app(app)
