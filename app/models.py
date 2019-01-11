363721ee164bbe6f6622ac20f3a6a8a79660f063
yleihan <yleihan@163.com>
2019-01-11 15:19:12 +0800

    8-end

--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from . import login_manager
 from flask_login import UserMixin
-
-
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer,primary_key = True)
@@ -21,6 +21,23 @@ class User(UserMixin,db.Model):
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String(64),unique=True ,index=True)
+    confirmed = db.Column(db.Boolean,default = False)
+
+    def generate_confirmation_token(self,expiration=3600):
+        s=Serializer(current_app.config['SECRET_KEY'],expiration)
+        return s.dumps({'confirm':self.id})
+
+    def confirm(self,token):
+        s=Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm')!=self.id:
+            return False
+        self.confirmed =True
+        db.session.add(self)
+        return True
 
     @property
     def password(self):
