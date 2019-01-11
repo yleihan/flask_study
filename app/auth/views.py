5833fbc4bdf85d4c22e0076a9fbf703879720bdd
yleihan <yleihan@163.com>
2019-01-11 11:08:14 +0800

    register

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,10 @@
 from flask import render_template,redirect,request,url_for,flash
 from . import auth
-from flask_login import login_user,login_required
+from flask_login import login_user,login_required,logout_user,current_user
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from ..email import send_email
+from .forms import LoginForm,RegistrationForm
 
 @auth.route('/login' , methods = ['GET','POST'])
 def login():
@@ -22,6 +24,16 @@ def logout():
     flash('you have been logged out ')
     return redirect(url_for('main.index'))
 
+
+@auth.route('/register',methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data,username=form.username.data,password=form.password.data)
+        db.session.add(user)
+        flash('You can now login')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html',form=form)
 
 
 
